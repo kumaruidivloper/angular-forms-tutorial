9ea6673a149c3dab5f2bc2746ed6260e13e27656
kumaruidivloper <kumaruidivloper@gmail.com>
2025-07-14 16:12:15 +1000

    Reactive-Form-validation

--- README.md
+++ README.md
@@ -10,25 +10,11 @@ To start a local development server, run:
 ng serve
 ```
 
-## Reactive Forms Part5 (FormValidation)
+## Reactive Forms Part5 (Creating Multi Step Reactive Form)
 
-<img width="1174" height="660" alt="image" src="https://github.com/user-attachments/assets/51d7dca1-6683-4bb2-aa4a-49bc55faa773" />
+<img width="1174" height="661" alt="image" src="https://github.com/user-attachments/assets/77373d4b-c810-4f07-8cec-3556571b0fa1" />
 
-<img width="1168" height="657" alt="image" src="https://github.com/user-attachments/assets/6e8a3172-6a49-4a33-bf33-b2f3dc58178d" />
+## Reactive Form Step 2
 
-<img width="1172" height="658" alt="image" src="https://github.com/user-attachments/assets/7a94752c-c794-4901-b184-ff97d391baaf" />
+<img width="918" height="645" alt="image" src="https://github.com/user-attachments/assets/0f330f86-0455-4b0d-a98c-7a39cc460024" />
 
-<img width="1171" height="657" alt="image" src="https://github.com/user-attachments/assets/6157df46-87b7-45ef-b27a-fc867db2be17" />
-
-<img width="1169" height="658" alt="image" src="https://github.com/user-attachments/assets/b4af3ea6-2877-4a5e-b2cd-75d1c87cbd0a" />
-
-<img width="1169" height="657" alt="image" src="https://github.com/user-attachments/assets/0522a9fc-7955-423b-894b-6324635b1111" />
-
-<img width="1172" height="660" alt="image" src="https://github.com/user-attachments/assets/46f9c7ea-990f-4fb2-8630-c29270ee00e0" />
-
-
-## FormValidation Q&A
-
-<img width="1173" height="657" alt="image" src="https://github.com/user-attachments/assets/6eb806fb-e257-4749-9fe6-88b711352b5f" />
-
-<img width="1173" height="659" alt="image" src="https://github.com/user-attachments/assets/097a51b2-0ebf-4f80-a98e-f8c113fef757" />
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,25 +8,41 @@ import {
   styleUrl: './app.scss'
 })
 export class App {
-  myForm: FormGroup;
-  nameControl: FormControl;
-  emailControl: FormControl;
+  myForm!: FormGroup
 
-  constructor() {
-    this.nameControl = new FormControl('', Validators.required);
-    this.emailControl = new FormControl('', [
-      Validators.required,
-      Validators.email,
-    ]);
-    this.myForm = new FormGroup({
-      name: this.nameControl,
-      email: this.emailControl,
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      userDetails: this.fb.group({
+        fname: ['', Validators.required],
+        // More the one validator we need assign this in array
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+      }),
+      additionalDetails: this.fb.group({
+        // More the one validator we need assign this in array
+        mobile: ['', [Validators.required, Validators.maxLength(10)]],
+        address: ['', Validators.required],
+        country: ['', Validators.required],
+        gender: ['', Validators.required],
+      }),
+      feedback: this.fb.group({
+        comments: ['', Validators.maxLength(100)],
+      })
     });
   }
 
-  formSubmit() {
-    if (this.myForm.valid) {
-      console.log('Form Values: ', this.myForm.value);
-    }
+  step: any = 1;
+
+  btnPrevious() {
+    this.step -= 1;
   }
+
+  btnNext() {
+    this.step += 1;
+  }
+
+  formSubmit() {
+    console.log(this.myForm.value)
+  }
+
 }
